efd6ac250167e12b3a96723aa6fb275cc4ecb011
Luka Stipic <ttl.luka@gmail.com>
2018-11-30 17:42:35 +0100

    second push

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,34 @@
 var handleContent = function () {
-    var contentDiv = '<div id="content">';
-    var clockDiv = '<div id="clock">';
-    var slider = '<input type="range" id="slider" value="100" max="150" min="50">';
 
-    $('body').prepend(contentDiv);
-    $('#content')
-        .addClass('content')
-        .append(slider)
-        .append(clockDiv);
-    $('#clock').addClass('clock');
+    var preloader = $('.preloader');
+    var ajaxLoader = '<img class="ajax-loader" src="/img/ajax-loader.gif" />';
 
-    var mouseDown = false;
-    $('#slider')
-        .mousedown(function () {
-            mouseDown = true;
-        })
-        .mouseup(function () {
-            mouseDown = false;
-        })
-        .mousemove(function () {
-            if (mouseDown) {
-                $('#clock').css('font-size', $(this).val() + 'px');
+    $('.btn').click(function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "data/users.json",
+            type: "GET",
+            data: { "id" :1},
+            beforeSend: function(){
+                preloader.empty();
+                preloader.append(ajaxLoader);
+            },
+            success: function(result){
+                
+            },
+            error: function(xhr, status, error){
+                if (error) {
+                    preloader.empty();
+                    preloader.text("An error occured while procssing your request! Your fault totaly!");
+                    
+                }
+            },
+            complete: function (){
+               // preloader.empty();
             }
         });
-
-    runClock();
-    var interval = setInterval(runClock, 1000);
-
-    function runClock() {
-        var date = new Date();
-        var hh = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
-        var mm = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
-        var ss = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-        $('#clock').text(hh + ':' + mm + ':' + ss);
-    }
+    });
 };
 
 var App = function () {
